130ed02b97af6e1691b949db3fc0fee71c5c5147
kvelraj_concord <kvelraj@concord.net>
2022-08-18 10:53:46 +0530

    javascript

---
+++ HTML/program.js
@@ -0,0 +1,9 @@
+// Number -> Any numberical value from Positive, Negative, Fractional, Decimals
+
+// To create a Variable (Temp storage)
+// Synax 
+// var Variable_Name = Value; 
+// Variable_Name -> Any valid text, it should be a-z, A-Z, 0-9, _, $
+
+var age = 28;
+console.log(age);
