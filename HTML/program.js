944f1bab38aa8b980ca60ade9aabe6a57abde2b4
kvelraj_concord <kvelraj@concord.net>
2022-08-23 10:51:07 +0530

    object data type

--- HTML/program.js
+++ HTML/program.js
@@ -7,3 +7,51 @@
 
 var age = 28;
 console.log(age);
+
+// String -> Any text value which is types inside '' or "" 
+
+var email_id_1 = "karthick123.afx@gmail.com";  
+
+var email_id_2 = "dummy@test.com";
+
+console.log(email_id_1);
+console.log(email_id_2);
+
+var text = 'This is my "book" Reference';
+console.log(text);
+
+// Boolean -> It should be true or false;
+var result = true;
+console.log(result);
+
+// undefined -> A variable will be initialized but value will not be assigned
+var future_result;
+console.log(future_result);
+
+
+//Student Informations
+var first_name = "karthick";
+var last_name = "kumar";
+var roll_number = "2335DFEG454";
+var english_score = 75;
+var math_score = 80;
+
+// Object -> It is a collection of key and value (collection of property)
+// syntax
+// var variable_name = { 
+//   key_1 : value_1,
+//   key_2 : value_2
+// };
+
+// key -> It is similar to variable_name -> It should contains a-z, A-Z, 0-9, _, $
+// value -> Number, string, Boolean, undefined, object, array
+
+var student_information = {
+  first_name : "karthick",
+  last_name : "kumar",
+  roll_number : "2335DFEG454",
+  english_score : 78,
+  math_score: 85
+};
+
+console.log(student_information);
