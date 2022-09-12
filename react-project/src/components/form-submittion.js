ea650c6d270cb32532c7adf56f6a30e37e390fcf
kvelraj_concord <kvelraj@concord.net>
2022-09-12 09:57:19 +0530

    react event

--- react-project/src/components/form-submittion.js
+++ react-project/src/components/form-submittion.js
@@ -1,17 +1,26 @@
 import React from "react";
 
 function FormSubmittion(){
+  
+  function onLogin(){
+    alert("Hey your aboout the submit the login form");
+  }
+
+  function onHandleInput(event){
+    console.log(event.target.value);
+  }
+  
   return(
     <div className="form-content">
       <div className="space">
         <label className="label">Enter your Name</label>
-        <input type="text" className="input"/>
+        <input type="text" className="input" onChange={onHandleInput}/>
       </div>
       <div className="space">
         <label className="label">Enter your Password</label>
-        <input type="password" className="input"/>
+        <input type="password" className="input" onChange={onHandleInput}/>
       </div>
-      <button className="button">Sumbit</button>
+      <button className="button" onClick={() => onLogin() }>Sumbit</button>
     </div>
   )
 }
