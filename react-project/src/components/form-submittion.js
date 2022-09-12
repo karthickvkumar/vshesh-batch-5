c0240d2802fd4e3c688c6ae94872687db300c1fc
kvelraj_concord <kvelraj@concord.net>
2022-09-12 10:48:16 +0530

    form sumbit

--- react-project/src/components/form-submittion.js
+++ react-project/src/components/form-submittion.js
@@ -8,24 +8,29 @@ function FormSubmittion(){
   });
   
   function onLogin(){
-    alert("Hey your aboout the submit the login form");
+    alert(loginFrom.username + " " + loginFrom.password);
   }
 
   function onHandleInput(event){
-    console.log(event.target.value);
+    // console.log(event.target.value, event.target.name);
+    setLoginForm({...loginFrom, [event.target.name] : event.target.value });
   }
   
   return(
     <div className="form-content">
       <div className="space">
         <label className="label">Enter your Name</label>
-        <input type="text" className="input" onChange={onHandleInput}/>
+        <input type="text" className="input" onChange={onHandleInput} name="username"/>
       </div>
       <div className="space">
         <label className="label">Enter your Password</label>
-        <input type="password" className="input" onChange={onHandleInput}/>
+        <input type="password" className="input" onChange={onHandleInput} name="password"/>
       </div>
       <button className="button" onClick={() => onLogin() }>Sumbit</button>
+      <div>
+        <h2>The username is {loginFrom.username}</h2>
+        <h2>The password is {loginFrom.password}</h2>
+      </div>
     </div>
   )
 }
