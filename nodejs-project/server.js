038440de9a8e2925945d89c873776281505818ca
kvelraj_concord <kvelraj@concord.net>
2022-09-22 10:51:24 +0530

    GET API

--- nodejs-project/server.js
+++ nodejs-project/server.js
@@ -11,6 +11,38 @@ app.use(cors({
   origin: "*"
 }));
 
+var connection = mysql.createConnection({
+  host: "remotemysql.com",
+  user: "wym4khPjwJ",
+  password: "IpVePeo1GV",
+  database: "wym4khPjwJ",
+  port: 3306
+});
+
+connection.connect((error) => {
+  if(error){
+    throw error;
+  }
+  else{
+    console.log("MYSQL Database is connected successfully");
+  }
+})
+
+// http://localhost:4000/user/list
+app.get("/user/list", (request, response) => {
+  var sql_query = `SELECT * FROM karthick_kumar`;
+
+  connection.query(sql_query, (error, result) => {
+    if(error){
+      response.status(500).send(error);
+    }
+    else{
+      response.status(200).send(result);
+    }
+  })
+})
+
+
 
 http.listen(4000, () => {
   console.log("Node JS Server is running...");
